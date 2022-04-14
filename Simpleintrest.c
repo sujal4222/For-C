abd66c1ca05299a36ea541e2795249586e910566
ManikSingh29 <21bec087@nith.ac.in>
2022-04-14 20:19:00 +0530

    Simple intrest

--- Simpleintrest.c
+++ Simpleintrest.c
@@ -10,7 +10,7 @@
     Si=p*24*t/100;
     printf("Principle value=%f",p);
     printf("\nRate by Bank= 24");
-    printf("\nTime period=%d",t);
+    printf("\nTime  period=%d",t);
     printf("\nSimple Intrest=%f\n",Si);
     return 0;
 }    
