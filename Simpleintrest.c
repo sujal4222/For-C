f70596585ff582100c663a4ce917e51801db1dae
ManikSingh29 <21bec087@nith.ac.in>
2022-04-15 11:06:04 +0530

    ADD

--- Simpleintrest.c
+++ Simpleintrest.c
@@ -9,7 +9,7 @@
     scanf("%d",&t);
     Si=p*24*t/100;
     printf("Principle value=%f",p);
-    printf("\nRate by Bank= 24");
+    printf("\nRate by Bank= 24%%");
     printf("\nTime  period=%d",t);
     printf("\nSimple Intrest=%f\n",Si);
     return 0;
