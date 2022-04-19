3cab811240cd5ed86b8ca9a7a7d4641cc74fd2c2
ManikSingh29 <21bec087@nith.ac.in>
2022-04-20 00:10:20 +0530

    Corrected

--- Clast.cpp
+++ Clast.cpp
@@ -1,42 +1,41 @@
 #include <stdio.h>
 int main()
 {
-	  char ch;
-	   int sum,sub,mul,mod;
-	printf("Enter first No.\n");
+	char ch='A';
+    int sum,sub,mul,mod;
+	float div;
+		printf("Enter The operation\n A- Addition \n S- Subtraction \n D-Division \n M-Multiplication \n R- Modulus\n");
+	  scanf("%c",&ch);
+	printf("\nEnter first No.\n");
 	int num1;
-	scanf("%d",&num1);                    // Inputs Radius from user
+	scanf("%d",&num1);              
     printf("Enter second No.\n");
-	int num2;
-	scanf("%d",&num1); 
-	   printf("Enter The operation\n A- Addition \n S- Subtraction \n D-Division \n M-Multiplication \n R- Modulus\n");
-	   scanf("%c",&ch);
+	int num2=1;
+	scanf("%d",&num2); 
+	   printf("%c",ch);
 	    sum=num1+num2;
 	    sub=num1-num2;
-	   float div=num1/num2;
+	   div=(float)num1/num2;
 	    mul=num1*num2;
 	    mod=num1%num2;
 	   switch(ch)
 	   {
 	   	case 'A':
-	   		  printf("\n Sum=",sum);
+	   		  printf("\n Sum= %d",sum);
 	   		  break;
-	   		case 'S':
-	   		  printf("\n Difference=",sub);
+	   	case 'S':
+	   		  printf("\n Difference= %d",sub);
 	   		  break;
-				 	case 'D':
-	   		  printf("\n Division=",div);
+		case 'D':
+	   		  printf("\n Division= %f",div);
 	   		  break;
-				 	case 'M':
-	   		  printf("\n Multiplication=",mul);
+	 	case 'M':
+	   		  printf("\n Multiplication= %d",mul);
 	   		  break;	  	
-	   		  	case 'R':
-	   		  printf("\n Remainder=",mod);
+	  	case 'R':
+	   		  printf("\n Remainder= %d",mod);
 	   		  break;
-	   		  
-	   		  default:
-	   		  	printf("Error");
-	   }
+	   		}
 	return 0;
 }
 	
