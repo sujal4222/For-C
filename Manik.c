7ef53938e76fafbbf9f06950781d61dc92fb6e51
ManikSingh29 <21bec087@nith.ac.in>
2022-04-21 23:01:20 +0530

    T2

--- Manik.c
+++ Manik.c
@@ -3,7 +3,7 @@ int main()
 {
 	printf("Enter radius\n");
 	int radius;
-	scanf("%d",&radius);                    // Inputs Radius from user
+	scanf("%d",&radius);                     // Inputs Radius from user
 	
 	return 0;
 }
