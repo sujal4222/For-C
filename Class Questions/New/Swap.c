3aed7b82e8cda4cf3d7c98f28f24c822d93954c5
ManikSingh29 <97960222+ManikSingh29@users.noreply.github.com>
2022-05-10 22:55:16 +0530

    Update Swap.c

--- Class Questions/New/Swap.c
+++ Class Questions/New/Swap.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-int swap(int* x,int* y)
+int swap(int *x,int *y)
 {
   int z;
     z=*x;
     *x=*y;
     *y=z;
-    printf("I have %d Choclates and %d cookies",x,y);
+  
 }
 
- main()
+ int main()
 {
-    printf("I have 'X' Choclates and 'Y' cookies");
+    printf("I have 'X' Choclates and 'Y' cookies\n");
     printf("Enter value for 'X'");
     int a;
     scanf("%d",&a);
-    printf("Enter value for 'y'");
+    printf("\nEnter value for 'y'");
     int b;
     scanf("%d",&b);
-     printf("I have %d Choclates and %d cookies",a,b);
-    swap(a,b);
-}
+    swap(&a,&b);
+     printf("\nI have %d Choclates and %d cookies",a,b);
+     return 0;
+    }
